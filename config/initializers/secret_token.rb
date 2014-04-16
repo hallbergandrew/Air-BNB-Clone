dfd46ffd84758c966d38e5259f51b4e0b366144b
Bonnie Bright & Andrew Michael Hallberg <student@epicodus.com>
2014-04-16 09:08:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirbnbClone::Application.config.secret_key_base = 'b9b36eda74d90cc515abc18e2af74387a1d9cb645552e2458003b192363185d1d384a3ae7d21a4aebcc846b426c119080b39e15b5f882328859fdb549e343bc7'
